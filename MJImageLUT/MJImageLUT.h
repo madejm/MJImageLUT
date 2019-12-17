66f5c476729c38ec19f53da2aa0fffd9dc467993
Mateusz Madej <madejmt@gmail.com>
2019-12-18 00:58:05 +0100

    Initial Commit

---
+++ MJImageLUT/MJImageLUT.h
@@ -0,0 +1,19 @@
+//
+//  MJImageLUT.h
+//  MJImageLUT
+//
+//  Created by Mateusz Madej on 18/12/2019.
+//  Copyright © 2019 Mateusz Madej. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MJImageLUT.
+FOUNDATION_EXPORT double MJImageLUTVersionNumber;
+
+//! Project version string for MJImageLUT.
+FOUNDATION_EXPORT const unsigned char MJImageLUTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJImageLUT/PublicHeader.h>
+
+
